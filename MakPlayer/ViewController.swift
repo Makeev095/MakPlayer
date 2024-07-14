05111f915bd90a0d2daf8d9c78ec6863331e139d
Dmitriy Makeev <makeev095095@mail.ru>
2024-07-14 18:02:01 +0300

    Initial Commit

---
+++ MakPlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MakPlayer
+//
+//  Created by Макей 😈 on 14.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
