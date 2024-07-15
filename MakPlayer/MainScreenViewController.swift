97a4b0f0673b56503dfa12cfd334d16675b3269a
Dmitriy Makeev <makeev095095@mail.ru>
2024-07-15 17:32:10 +0300

    Added AvFoundation, but player doesn't work

--- MakPlayer/MainScreenViewController.swift
+++ MakPlayer/MainScreenViewController.swift
@@ -6,19 +6,42 @@
 //
 
 import UIKit
+import AVFoundation
 
 class MainScreenViewController: UIViewController {
     
-    private var backgroundImageView = UIImageView()
-    private var albumImageView = UIImageView()
-    private var songLabel = UILabel()
-    private var playButton = UIButton()
-
+    private let backgroundImageView = UIImageView()
+    private let albumImageView = UIImageView()
+    private let songLabel = UILabel()
+    private let playButton = UIButton()
+    private let pauseButton = UIButton()
+    private var player: AVAudioPlayer?
+    private var isPlaying = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let url = URL(string: "https://swifteasy.ru/lessons/App003/start.mp3")
+        downloadAudio(from: url!)
+        
         setupViewForMainScreen()
         setupConstraintsForMainScreen()
+    }
+    
+    private func downloadAudio(from url: URL) {
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data, error == nil else {
+                print(error?.localizedDescription ?? "Unknown error")
+                return
+            }
+            
+            do {
+                self.player = try AVAudioPlayer(data: data)
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+        task.resume()
     }
     
     private func setupViewForMainScreen() {
@@ -37,12 +60,17 @@ class MainScreenViewController: UIViewController {
         
         playButton.setImage(UIImage(systemName: "play.fill"), for: .normal)
         playButton.contentMode = .scaleAspectFill
-//        playButton.addTarget(self, action: #, for: .touchUpInside)
+        playButton.addTarget(self, action: #selector(playButtonAction), for: .touchUpInside)
+        
+        pauseButton.setImage(UIImage(systemName: "pause.fill"), for: .normal)
+        pauseButton.contentMode = .scaleAspectFill
+        pauseButton.addTarget(self, action: #selector(pauseButtonAction), for: .touchUpInside)
         
         view.addSubview(backgroundImageView)
         backgroundImageView.addSubview(albumImageView)
         backgroundImageView.addSubview(songLabel)
         backgroundImageView.addSubview(playButton)
+        backgroundImageView.addSubview(pauseButton)
         
     }
     
@@ -52,7 +80,8 @@ class MainScreenViewController: UIViewController {
         albumImageView.translatesAutoresizingMaskIntoConstraints = false
         songLabel.translatesAutoresizingMaskIntoConstraints = false
         playButton.translatesAutoresizingMaskIntoConstraints = false
-
+        pauseButton.translatesAutoresizingMaskIntoConstraints = false
+        
         NSLayoutConstraint.activate([
             backgroundImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             backgroundImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
@@ -70,17 +99,24 @@ class MainScreenViewController: UIViewController {
             songLabel.trailingAnchor.constraint(equalTo: albumImageView.trailingAnchor),
             
             playButton.topAnchor.constraint(equalTo: songLabel.bottomAnchor, constant: 15),
-//            playButton.leadingAnchor.constraint(equalTo: songLabel.leadingAnchor, constant: 100),
+            playButton.leadingAnchor.constraint(equalTo: songLabel.leadingAnchor, constant: 100),
             playButton.widthAnchor.constraint(equalToConstant: 100),
-            playButton.heightAnchor.constraint(equalToConstant: 100)
+            playButton.heightAnchor.constraint(equalToConstant: 100),
             
-
+            pauseButton.topAnchor.constraint(equalTo: songLabel.bottomAnchor, constant: 15),
+            pauseButton.trailingAnchor.constraint(equalTo: songLabel.trailingAnchor, constant: 100),
+            pauseButton.widthAnchor.constraint(equalToConstant: 100),
+            pauseButton.heightAnchor.constraint(equalToConstant: 100)
         ])
     }
     
     @objc private func playButtonAction() {
-        
+        player?.play()
+        isPlaying = true
     }
     
-    // TEST ON MACBOOK
+    @objc private func pauseButtonAction() {
+        player?.pause()
+        isPlaying = false
+    }
 }
